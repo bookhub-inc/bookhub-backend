3864f3e26792305e1ff00a432595e385f1f6e1db
Arthur Rio <thurrio@gmail.com>
2019-09-21 12:37:37 -0300

    Revert "Revert "Primeiro PR""

---
+++ src/main/java/com/bookhub/backendbookhub/entity/ComentarioEntity.java
@@ -0,0 +1,19 @@
+package com.bookhub.backendbookhub.entity;
+
+/**
+ * @author Arthur Rio
+ * @since 9/16/19
+ */
+//@Entity
+//@Table(name = "COMENTARIO")
+public class ComentarioEntity {
+
+// @Id
+// @Column(name = "ID_COMENTARIO")
+ private Long id;
+
+
+
+
+}
+
---
+++ src/test/java/com/bookhub/backendbookhub/BackendBookhubApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bookhub.backendbookhub;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BackendBookhubApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
