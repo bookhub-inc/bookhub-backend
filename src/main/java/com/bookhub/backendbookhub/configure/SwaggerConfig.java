867d8e6d327dd7c56e19b2038a2a4d63099a552b
Arthur Rio <thurrio@gmail.com>
2019-10-29 23:07:31 -0300

    Adicionando configuracoes do springfox

--- src/main/java/com/bookhub/backendbookhub/configure/SwaggerConfig.java
+++ src/main/java/com/bookhub/backendbookhub/configure/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.bookhub.backendbookhub.swagger;
+package com.bookhub.backendbookhub.configure;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
