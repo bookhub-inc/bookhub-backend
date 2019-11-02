9c3a147a72658e8199425b188e72f3b458c52633
herodek <herodek_10@hotmail.com>
2019-11-02 18:09:33 -0300

    Criacao do banco

--- src/main/flyway/scripts/V001__init.sql
+++ src/main/flyway/scripts/V001__init.sql
@@ -1,25 +1,124 @@
-CREATE USER BKH_ADM WITH
-PASSWORD 'bkh_admn'
-CONNECTION LIMIT 20
-VALID UNTIL 'infinity';
+CREATE USER 'BKH_ADM'
+identified by 'bkh_admn';
 
-GRANT ALL PRIVILEGES ON SCHEMA public TO BKH_ADM;
-GRANT ALL PRIVILEGES ON SCHEMA BKH_ADM TO BKH_ADM;
-SET search_path TO BKH_ADM,public;
+create database BKH_ADM;
 
-CREATE USER MANUT_BKH WITH
-CONNECTION LIMIT 20
-VALID UNTIL 'infinity'
-PASSWORD 'manut_bkh';
+use BKH_ADM;
 
-GRANT USAGE ON SCHEMA public TO MANUT_BKH;
-GRANT USAGE ON SCHEMA BKH_ADM TO MANUT_BKH;
+GRANT ALL PRIVILEGES ON BKH_ADM TO 'BKH_ADM';
 
-ALTER DEFAULT PRIVILEGES IN SCHEMA BKH_ADM GRANT ALL PRIVILEGES ON TABLES TO BKH_ADM;
-ALTER DEFAULT PRIVILEGES IN SCHEMA BKH_ADM GRANT ALL PRIVILEGES ON SEQUENCES TO BKH_ADM;
-ALTER DEFAULT PRIVILEGES IN SCHEMA BKH_ADM GRANT ALL PRIVILEGES ON FUNCTIONS TO BKH_ADM;
-ALTER ROLE BKH_ADM SET search_path TO BKH_ADM,public;
+Create table Usuario(
+Id int primary key,
+Nome varchar(30) not null,
+Sobrenome varchar(30) not null,
+Facebook varchar(40),
+Telefone varchar(14) UNIQUE,
+Relacionamento char,
+Usuario varchar(25) not null,
+Senha varchar(40) not null,
+Cpf varchar(11) unique,
+Dta_criacao datetime not null,
+Dta_ultacesso datetime not null
+);
 
-ALTER DEFAULT PRIVILEGES IN SCHEMA BKH_ADM GRANT SELECT ON TABLES TO MANUT_BKH;
-ALTER DEFAULT PRIVILEGES IN SCHEMA BKH_ADM GRANT SELECT, USAGE ON SEQUENCES TO MANUT_BKH;
-ALTER DEFAULT PRIVILEGES IN SCHEMA BKH_ADM GRANT EXECUTE ON FUNCTIONS TO MANUT_BKH;
+create table Livro_favorito(
+Id_usuario int,
+Usuario varchar(25),
+Id_livro int,
+Nome_livro varchar(80));
+
+create table Livro(
+Id int primary key,
+Nome varchar(80) not null,
+Autor varchar(40) not null,
+Publicadora varchar(60),
+Descricacao text,
+Dta_lancamento date,
+N_paginas int
+);
+
+create table LivXcat(
+Id_livro int,
+Id_cat int
+);
+
+create table Categoria(
+Id int primary key,
+Categoria varchar(40)
+);
+
+create table Comentario(
+Id int primary key,
+Comentario varchar(500),
+Id_usuario int,
+Usuario varchar(25),
+Rating double,
+Dta_comentario date,
+Id_livro int
+);
+
+create table Topico(
+Id int primary key,
+Titulo varchar (60),
+Id_usuario int,
+Usuario varchar(25)
+);
+
+create table Topico_comentario(
+Id int primary key,
+Comentario varchar(500),
+Dta_comentario datetime,
+Id_usuario int,
+Usuario varchar(25),
+Id_topico int);
+
+
+alter table Livro_favorito
+add foreign key (Id_usuario) references Usuario(Id),
+add foreign key (Id_livro) references Livro(Id);
+
+alter table LivXcat
+add foreign key (Id_livro) references Livro(Id),
+add foreign key (Id_cat) references Categoria(Id);
+
+alter table Comentario
+add foreign key (Id_usuario) references Usuario(Id),
+add foreign key (Id_livro) references Livro(Id);
+
+alter table Topico_comentario
+add foreign key (Id_topico) references Topico(Id);
+
+use bkh_adm;
+
+alter table Usuario modify column Senha varbinary(255);
+
+Set SQL_SAFE_UPDATES = 0;
+
+Update Usuario set Senha = aes_encrypt(Senha,64);
+
+
+use bkh_adm;
+
+alter table Topico
+add column Dta_Criacao datetime after Titulo;
+
+Create table Usuario_Livro(
+Id int primary key,
+Id_Usuario int,
+Usuario varchar (20),
+Id_Livro int,
+Nome varchar(60),
+ foreign key (Id_usuario) references Usuario(Id),
+ foreign key (Id_Livro) references Livro(Id));
+ 
+ Create table Usuario_Estante(
+ Id int primary key,
+ Id_Usuario Int,
+ Id_Livro Int,
+ Nome varchar(60),
+ Lido binary,
+ Comprado binary,
+ foreign key (Id_Usuario) references Usuario(Id),
+ foreign key (Id_Livro) references Livro(Id));
+ 
+ drop table Livro_favorito;
--- src/main/flyway/scripts/V003__init.sql
+++
@@ -1,7 +0,0 @@
-use bkh_adm;
-
-alter table Usuario modify column Senha varbinary(255);
-
-Set SQL_SAFE_UPDATES = 0;
-
-Update Usuario set Senha = aes_encrypt(Senha,64);
