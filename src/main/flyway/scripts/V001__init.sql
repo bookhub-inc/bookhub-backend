7f5e6ee74c031e1329144b733f5f616cb0f498de
herodek <herodek_10@hotmail.com>
2019-11-02 21:13:29 -0300

    Criação usuário

--- src/main/flyway/scripts/V001__init.sql
+++ src/main/flyway/scripts/V001__init.sql
@@ -1,13 +1,10 @@
-CREATE USER 'bkh_adm'@'%'
-identified by 'bkh_admn';
-
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, RELOAD, PROCESS, REFERENCES, INDEX, ALTER, SHOW DATABASES, CREATE TEMPORARY TABLES, LOCK TABLES, EXECUTE, REPLICATION SLAVE, REPLICATION CLIENT, CREATE VIEW, SHOW VIEW, CREATE ROUTINE, ALTER ROUTINE, CREATE USER, EVENT, TRIGGER ON *.* TO 'bkh_adm'@'%' WITH GRANT OPTION;
-
 create database bkh_adm;
 
 use bkh_adm;
 
-Create table Usuario(
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, RELOAD, PROCESS, REFERENCES, INDEX, ALTER, SHOW DATABASES, CREATE TEMPORARY TABLES, LOCK TABLES, EXECUTE, REPLICATION SLAVE, REPLICATION CLIENT, CREATE VIEW, SHOW VIEW, CREATE ROUTINE, ALTER ROUTINE, CREATE USER, EVENT, TRIGGER ON *.* TO 'bkh_adm'@'%' WITH GRANT OPTION;
+
+Create table usuario(
 id int primary key auto_increment,
 nome varchar(30) not null,
 sobrenome varchar(30) not null,
@@ -21,27 +18,27 @@ dta_criacao datetime not null,
 dta_ultacesso datetime not null
 );
 
-create table Livro(
-Id int primary key,
-Nome varchar(80) not null,
-Autor varchar(40) not null,
-Publicadora varchar(60),
-Descricao text,
-Dta_lancamento date,
-N_paginas int
+create table livro(
+id int primary key,
+nome varchar(80) not null,
+autor varchar(40) not null,
+publicadora varchar(60),
+descricao text,
+dta_lancamento date,
+n_paginas int
 );
 
-create table LivXcat(
+create table livxcat(
 id_livro int not null,
 id_cat int not null
 );
 
-create table Categoria(
+create table categoria(
 id int primary key auto_increment,
 nome_categoria varchar(40)
 );
 
-create table Comentario(
+create table comentario(
 id int primary key,
 comentario varchar(500),
 id_usuario int not null,
@@ -50,7 +47,7 @@ dta_criacao datetime,
 id_livro int
 );
 
-create table Topico(
+create table topico(
 id int primary key,
 titulo varchar (60),
 id_usuario int not null,
@@ -58,7 +55,7 @@ usuario varchar(25),
 dta_criacao date
 );
 
-create table Topico_comentario(
+create table topico_comentario(
 id int primary key not null,
 comentario varchar(500),
 dta_comentario datetime,
@@ -71,29 +68,22 @@ add foreign key (id_livro) references livro(id),
 add foreign key (id_cat) references categoria(id);
 
 alter table comentario
-add foreign key (id_usuario) references Usuario(id),
-add foreign key (id_livro) references Livro(id);
+add foreign key (id_usuario) references usuario(id),
+add foreign key (id_livro) references livro(id);
 
 alter table topico_comentario
 add foreign key (id_topico) references topico(id);
 
-use bkh_adm;
-
 alter table usuario modify column senha varbinary(255);
 
-set SQL_SAFE_UPDATES = 0;
+set sql_safe_updates = 0;
 
-update Usuario set Senha = aes_encrypt(senha,64);
+update usuario set senha = aes_encrypt(senha,64);
 
-use bkh_adm;
-
-alter table topico
-add column dta_criacao datetime after titulo;
-
-create table Usuario_Estante(
+create table usuario_estante(
 id int primary key,
-id_usuario Int,
-id_livro Int,
+id_usuario int,
+id_livro int,
 lido boolean,
 comprado boolean,
 gostou boolean,
