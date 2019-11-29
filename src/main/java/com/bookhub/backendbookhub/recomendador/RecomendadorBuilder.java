02652072fce06d4446c15659d5ca48688c95a62b
Arthur Rio <thurrio@gmail.com>
2019-11-29 17:25:44 -0300

    Removendo imports nao utilizados

--- src/main/java/com/bookhub/backendbookhub/recomendador/RecomendadorBuilder.java
+++ src/main/java/com/bookhub/backendbookhub/recomendador/RecomendadorBuilder.java
@@ -3,7 +3,6 @@ package com.bookhub.backendbookhub.recomendador;
 import org.apache.mahout.cf.taste.common.TasteException;
 import org.apache.mahout.cf.taste.eval.RecommenderBuilder;
 import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
-import org.apache.mahout.cf.taste.impl.recommender.GenericBooleanPrefUserBasedRecommender;
 import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
 import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
